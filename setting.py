db562f47efa2a6bd42deaa4fb9fde35b183709f7
aria rahman <ariarahman010@gmail.com>
2026-03-20 15:01:27 +0600

    background image for the game

--- setting.py
+++ setting.py
@@ -1,16 +1,30 @@
 import pygame 
 from pygame.locals import *
 
-screen_width = 800
-screen_height= 650
+
+clock = pygame.time.Clock()
+fps=60
+
+
+screen_width = 1000
+screen_height= 700
 screen= pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption("SPACE SHOOTER")
 
-#game_background=pygame.image.load()
+game_background=pygame.image.load("images/space.jpg")
+
+
+def draw_bg():
+    screen.blit(game_background,(0,0))
 
 run=True
 while run:
+    clock.tick(fps)
+    draw_bg()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run=False
+
+
+    pygame.display.update()
 pygame.quit()
