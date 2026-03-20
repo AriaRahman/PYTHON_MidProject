f65f6f21273361dfe4d3d7f2133acd2477066076
aria rahman <ariarahman010@gmail.com>
2026-03-20 18:10:19 +0600

    adding health feature

--- setting.py
+++ setting.py
@@ -6,11 +6,14 @@ clock = pygame.time.Clock()
 fps=60
 
 
-screen_width = 1000
+screen_width = 1060
 screen_height= 700
 screen= pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption("SPACE SHOOTER")
 
+
+red= (255,0,0)
+green= (0,255,0)
 game_background=pygame.image.load("images/space.jpg")
 
 
@@ -20,7 +23,7 @@ def draw_bg():
 
 
 class Spaceship(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, x, y, health):
         pygame.sprite.Sprite.__init__(self)
 
         img = pygame.image.load("images/spaceship.png").convert_alpha()
@@ -33,23 +36,35 @@ class Spaceship(pygame.sprite.Sprite):
                 if r > 200 and g > 200 and b > 200:
                     img.set_at((x1, y1), (0, 0, 0, 0))  
         
-        self.image = pygame.transform.scale(img, (450, 120))
-        
+        self.image = pygame.transform.scale(img, (150, 70))        
         self.rect = self.image.get_rect()
         self.rect.midbottom = (x, y)
+        self.health_start = health
+        self.health_remaining = health
+    
 
-# class Spaceship(pygame.sprite.Sprite):
-#     def __init__(self,x,y):
-#         pygame.sprite.Sprite.__init__(self)
-#         self.image = pygame.image.load("images/spaceship.png")
-#         self.rect = self.image.get_rect()
-#         self.rect.center = [x,y]
+ 
+    def update(self):
+        speed = 8
+        key= pygame.key.get_pressed()
+        if key[pygame.K_LEFT] and self.rect.left > 0:
+            self.rect.x -= speed
+        if key[pygame.K_RIGHT] and self.rect.right < screen_width:
+            self.rect.x += speed
+        
+        pygame.draw.rect(screen, red, (self.rect.x , (self.rect.bottom + 10), self.rect.width, 15))
+        if self.health_remaining > 0 :
+                 pygame.draw.rect(screen,green , (self.rect.x , (self.rect.bottom + 10), int(self.rect.width * (self.health_remaining/ self.health_start)), 15))
+
+
+
+
+
 
 spaceship_group = pygame.sprite.Group()
 
-spaceship= Spaceship(int(screen_width / 2),screen_height - 100)
+spaceship= Spaceship(int(screen_width / 2),screen_height - 100 , 3)
 spaceship_group.add(spaceship)
-
 
 
 run=True
@@ -59,6 +74,9 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run=False
+
+
+    spaceship.update()        
     spaceship_group.draw(screen)
 
     pygame.display.update()
